548a06dd6b629669b6a9ed2eb4ceee87612642ff
Stefan Hausotte <stefan.hausotte@gdata.de>
2018-02-26 08:43:04 +0100

    fixed broken linux build bootstrapper

--- CakeApp/Content/build.sh
+++ CakeApp/Content/build.sh
@@ -7,13 +7,14 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
 CAKE_VERSION=0.23.0
+NUGET_URL="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 CAKE_FOLDER=$TOOLS_DIR/Cake.CoreCLR/$CAKE_VERSION/
 CAKE_DLL=$CAKE_FOLDER/Cake.dll
 ADDIN_PATH=$TOOLS_DIR/Addins
 
 DOCKER_ADDIN_URI="https://www.nuget.org/api/v2/package/Cake.Docker/0.8.3"
-DOCKER_ADDIN_PATH=$ADDIN_PATH/Core.Docker/lib/netstandard1.6/Cake.Docker.dll
 DOCKER_NUPGK=$ADDIN_PATH/Cake.Docker.nupkg
+NUGET_EXE_PATH=$TOOLS_DIR/nuget.exe
 
 # Define default arguments.
 TARGET="Default"
@@ -38,7 +39,20 @@ done
 # Make sure the tools folder exist.
 if [ ! -d "$TOOLS_DIR" ]; then
     mkdir "$TOOLS_DIR"
+	mkdir "$ADDIN_PATH"
 fi
+
+###########################################################################
+# ADD DOCKER PLUGIN
+###########################################################################
+
+if [ ! -f "$DOCKER_NUPGK" ]; then
+    echo "Downloading Cake.Docker"
+    wget $DOCKER_ADDIN_URI -O $DOCKER_NUPGK
+	mkdir -p $ADDIN_PATH/cake.docker.0.8.3/Cake.Docker
+    unzip $DOCKER_NUPGK -d $ADDIN_PATH/cake.docker.0.8.3/Cake.Docker
+fi
+
 
 ###########################################################################
 # INSTALL .NET CORE CLI
@@ -51,6 +65,13 @@ fi
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 dotnet --info
+
+###########################################################################
+# INSTALL NUGET
+###########################################################################
+if [ ! -f "$NUGET_EXE_PATH" ]; then
+	wget -O $NUGET_EXE_PATH $NUGET_URL
+fi
 
 ###########################################################################
 # INSTALL CAKE
@@ -71,23 +92,6 @@ if [ ! -f "$CAKE_DLL" ]; then
     # Remove the zip
     rm -f $ZIP_NAME
 fi
-
-# Make sure that Cake has been installed.
-# if [ ! -f "$CAKE_DLL" ]; then
-#     echo "Could not find Cake.exe at '$CAKE_DLL'."
-#     exit 1
-# fi
-
-###########################################################################
-# ADD DOCKER PLUGIN
-###########################################################################
-
-if [ ! -f "$DOCKER_NUPGK" ]; then
-    echo "Downloading Cake.Docker"
-    wget -O $DOCKER_NUPGK $DOCKER_ADDIN_URI
-    unzip $DOCKER_NUPGK -d $ADDIN_PATH/Cake.Docker
-fi
-
 
 ###########################################################################
 # RUN BUILD SCRIPT
